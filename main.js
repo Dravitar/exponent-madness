0c9e403a77518a5b57dd6204de7f6871151db050
Alex Henderson <bohroco@aol.com>
2018-11-21 17:27:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var num = 2; // number that we are increasing
+var num = 1; // number that we are increasing
 var abbv = " "; // abbreviation according to AAS
 var countdown = 0; // counter for the button cooldown
-var notationList1 = [" ", "U", "D", "T", "Qa", "Qi", "Sx", "Sp", "Oc", "N"];
+//var notationList1 = [" ", "U", "D", "T", "Qa", "Qi", "Sx", "Sp", "Oc", "N"];
 //var notationList2 = ["D", "Vg", "Tg", "Qg", "Qq", "Sg", "Su", "Og", "Ng"];
 function update(set,get){ // for updating display
 	document.getElementById(set).innerHTML=get;
