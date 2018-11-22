68b4bbed3e878e3c984223dff8859108aef90dcb
pg132 <42481851+pg132@users.noreply.github.com>
2018-11-21 23:07:57 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,43 @@
+var user = {
+	number:1,
+	clickMult:1.5,
+	currentMaxNumber:1e33,
+	lastClick: new Time().getTime(),
+	clickTime:3000
+	
+}
+
+
+
+
+
 var num = 1; // number that we are increasing
 var abbv = " "; // abbreviation according to AAS
 var countdown = 0; // counter for the button cooldown
 var notationList1 = [" " ,"K", "M", "B", "T", "Qa", "Qi", "Sx", "Sp", "Oc", "N"];
 //var notationList2 = ["D", "Vg", "Tg", "Qg", "Qq", "Sg", "Su", "Og", "Ng"];
-function update(set,get){ // for updating display
+function update1(set,get){ // for updating display
 	document.getElementById(set).innerHTML=get;
 }
+
+
+
+
 function step() { // clicks button
-	if(countdown==0) {
-		num = Math.round(num*1.5*100)/100; // updates number
-		//num = num*1.5;
-		if(num>1000) shorten(num, abbv);/////////THIS ONE/////////
+	if (user.number > user.currentMaxNumber) return
+	if(user.clickTime+user.lastClick <= new Time().getTime()) {
+		user.number = Math.round(user.number*user.clickMult*100)/100; // updates number
+		if(user.number>1000) shorten(num, abbv);/////////THIS ONE/////////
 		//update("numDisplay",num);
-		update("numDisplay",shorten(num)); // update number on the page
-		countdown = 3000; // reset cooldown timer
+		update("numDisplay",shorten(user.number)); // update number on the page
+		user.lastClick = new Time().getTime(); // reset cooldown timer
 	}
-	else return 0;
+	else return ;
 }
 	
 function updateCountdown() { // updates the cooldown timer
-	if(countdown>50) countdown = countdown-50;
-	if(countdown<=50&&countdown>=0) countdown = 0;
-	update("mainButton", "Milliseconds remaining:" + countdown);
+	var msDiff = Math.max(user.lastClick+user.clickTime-new Time().getTime(),0)
+	update1("mainButton", "Milliseconds remaining:" + msDiff);
 }
 	
 function shorten(number) {
